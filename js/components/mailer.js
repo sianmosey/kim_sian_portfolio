d2ed7c4455d4b80979544e8166558602beec39f4
sianmosey <90750078+sianmosey@users.noreply.github.com>
2022-12-02 15:01:17 -0500

    Add files via upload

---
+++ js/components/mailer.js
@@ -0,0 +1,27 @@
+async function SendMail(targetForm) {
+    // mail stuff goes here
+    let formData = new FormData(targetForm), 
+        formFieldErrors = false;
+
+    let result = await fetch(`./${targetForm.getAttribute("action")}`, {
+        method: targetForm.method,
+        body: formData,
+    })
+    .then(response => {
+        if (response.status !== 200) {
+            formFieldErrors = true;
+        }
+
+        return response;
+    })
+
+    let mailStatus = await result.json();
+
+    if (formFieldErrors) {
+        throw new Error(JSON.stringify(mailStatus));
+    }    
+    
+    return mailStatus;
+}
+
+export { SendMail };
